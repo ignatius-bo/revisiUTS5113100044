f8f50ef835f4b99b99d06b71a9ba603c76fbb262
ignatius-bo <inyas.benedict@gmail.com>
2015-12-15 17:05:56 +0700

    revisi 1.1

--- src/revisi/REVISI.java
+++ src/revisi/REVISI.java
@@ -48,12 +48,61 @@ public class REVISI {
         soal = input.readLine();
         System.out.println(soal);
         
-        
-        
-        
-        
-        
-        
+            while (true){
+                String split[] = soal.split(" ");
+                System.out.println(split[0]);
+                System.out.println(split[1]);
+                System.out.println(split[2]);
+
+                int angka1 = Integer.parseInt(split[0]);
+                int angka2 = Integer.parseInt(split[2]);
+                int hasil = 0;
+
+                if(split[1].equals("+")){
+                    System.out.println("masuk +");
+                    hasil = angka1 + angka2;        
+                }
+                else if(split[1].equals("-")){
+                    System.out.println("masuk -");
+                    hasil = angka1 - angka2;        
+                }
+                else if(split[1].equals("x")){
+                    System.out.println("masuk x");
+                    hasil = angka1 * angka2;        
+                }
+                else if(split[1].equals("mod")){
+                    System.out.println("masuk mod");
+                    hasil = angka1 % angka2;        
+                }
+
+                System.out.println(hasil);
+                String jawab = "Result:" + hasil + "\n";
+                System.out.println(jawab);
+                writer.write(jawab);
+                writer.flush();
+
+                test = input.readLine();
+                System.out.println(test);
+                if (test.equals("Hash:")){
+                    break;
+                }
+                soal = input.readLine();
+                System.out.println(soal);
+                
+            }
+            String leng = input.readLine();
+            System.out.println(leng);
+            String isi = input.readLine();
+            System.out.println(isi);
+            
+            String jawab = "Hash:" + isi + "\n";
+            System.out.println(jawab);
+            writer.write(jawab);
+            writer.flush();
+            
+            test = input.readLine();
+            System.out.println(test);
+
     }
     
 }
